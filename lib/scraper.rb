bdf3186f0147860b0d377e43241716344166b362
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-11 11:22:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,5 +9,3 @@ courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo00
 courses.each do |course|
   puts course.text.strip
 end
-
-p doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].name
